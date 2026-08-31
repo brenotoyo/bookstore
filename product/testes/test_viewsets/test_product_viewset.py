e8869aa9ae38da76b6d58b5173db8801bce4b28e
Breno Kazuo <breno_toyo@hotmail.com>
2026-08-31 18:42:02 -0300

    Adicionando a autenticação e corrigindo os testes

--- product/testes/test_viewsets/test_product_viewset.py
+++ product/testes/test_viewsets/test_product_viewset.py
@@ -2,6 +2,8 @@ import json
 
 from rest_framework.test import APIClient, APITestCase
 from rest_framework.views import status
+from rest_framework.authtoken.models import Token
+
 from django.urls import reverse
 
 from product.factories import CategoryFactory, ProductFactory
@@ -14,6 +16,8 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
 
         self.product = ProductFactory(
             title="pro controller",
@@ -21,6 +25,8 @@ class TestProductViewSet(APITestCase):
         )
 
     def test_get_all_product(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
         response = self.client.get(
             reverse("product-list",
                     kwargs={"version": "v1"}
@@ -30,11 +36,13 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         product_data = json.loads(response.content)
 
-        self.assertEqual(product_data[0]["title"], self.product.title)
-        self.assertEqual(product_data[0]["price"], self.product.price)
-        self.assertEqual(product_data[0]["active"], self.product.active)
+        self.assertEqual(product_data['results'][0]["title"], self.product.title)
+        self.assertEqual(product_data['results'][0]["price"], self.product.price)
+        self.assertEqual(product_data['results'][0]["active"], self.product.active)
 
     def test_create_product(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
         category = CategoryFactory()
         data = json.dumps({
             "title": "notebook",
