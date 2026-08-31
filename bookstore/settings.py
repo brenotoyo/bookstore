b7ae39d228b13b1422904beb9df89e894f19718b
Breno Kazuo <breno_toyo@hotmail.com>
2026-08-31 15:34:32 -0300

    Adicionando a paginação ao projeto

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-    'rest_framework',
-    'order',
-    'product',
+    "rest_framework",
+    "order",
+    "product",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
@@ -129,3 +131,12 @@ MAILERS = {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
